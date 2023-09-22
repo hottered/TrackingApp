16581106cdcfa83c083f28b7302db3551a1a8e2f
Aleksa <aleksa.mitrovic2001@gmail.com>
2023-09-22 22:27:00 +0200

    ImplementedTheRunningTimeStop

---
+++ app/src/main/java/com/example/runningapp/di/ServiceModule.kt
@@ -0,0 +1,58 @@
+﻿package com.example.runningapp.di
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.example.runningapp.R
+import com.example.runningapp.other.Constance
+import com.example.runningapp.ui.MainActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @ServiceScoped //only one Instance of the providerClinet in the service
+    @Provides
+    fun provideFusedLocationProviderClient(
+        @ApplicationContext app: Context
+    ) = FusedLocationProviderClient(app)
+
+    @ServiceScoped
+    @Provides
+    fun provideMainActivityPendingIntent(
+        @ApplicationContext app: Context
+    ) = PendingIntent.getActivity(
+        app,
+        0,
+        Intent(
+            app, MainActivity::class.java
+        ).also {
+            it.action = Constance.ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        PendingIntent.FLAG_UPDATE_CURRENT //wehener we lauch the intent it will be updated instead of creating again
+    )
+    
+    @ServiceScoped
+    @Provides
+    fun provideBaseNotificationBuilder(
+        @ApplicationContext app: Context,
+        pendingIntent: PendingIntent
+    ) = NotificationCompat.Builder(app,
+        Constance.NOTIFICATION_CHANNEL_ID
+    )
+        .setAutoCancel(false)//prevent that user click on notification to be active prevent cancalable
+        .setOngoing(true) // can be swiped away
+        .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+        .setContentTitle("Running App")
+        .setContentText("00:00:00")
+        .setContentIntent(pendingIntent)
+}
--- app/src/main/java/com/example/runningapp/services/TrackingService.kt
+++ app/src/main/java/com/example/runningapp/services/TrackingService.kt
@@ -8,6 +8,7 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.database.CursorWindowAllocationException
 import android.location.Location
 import android.os.Build
 import android.os.Looper
@@ -34,22 +35,32 @@ import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.model.LatLng
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.inject.Inject
 
 typealias Polyline = MutableList<LatLng>
 typealias Polylines = MutableList<Polyline>
 
+@AndroidEntryPoint
 class TrackingService : LifecycleService() {
 
     var isFirstRun = true
 
+    @Inject
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     private val timeRunInSeconds = MutableLiveData<Long>()
+    
+    @Inject
+    lateinit var baseNotificationBuilder:NotificationCompat.Builder
+    
+    
+    lateinit var curNotificationBuilder:NotificationCompat.Builder
 
     companion object {
         val timeRunInMillis = MutableLiveData<Long>()
@@ -59,10 +70,12 @@ class TrackingService : LifecycleService() {
 
     override fun onCreate() {
         super.onCreate()
+        curNotificationBuilder = baseNotificationBuilder
         postInitialValues()
         fusedLocationProviderClient = FusedLocationProviderClient(this)
         isTracking.observe(this, Observer {
             updateLocationTracking(it)
+            updateNotificationTrackingState(it)
         })
     }
 
@@ -137,6 +150,28 @@ class TrackingService : LifecycleService() {
         timeRunInMillis.postValue(0L)
     }
 
+    private fun updateNotificationTrackingState(isTracking: Boolean){
+        val notificationActionText = if(isTracking) "Pause" else "Resume"
+        val pendingIntent = if(isTracking) {
+            val pauseIntent = Intent(this,TrackingService::class.java).apply { 
+                action = ACTION_PAUSE_SERVICE
+            }
+            PendingIntent.getService(this,1,pauseIntent, FLAG_UPDATE_CURRENT)
+        }else{
+            val resumeIntent = Intent(this,TrackingService::class.java).apply { 
+                action = ACTION_START_OR_RESUME_SERVICE
+            }
+            PendingIntent.getService(this,2,resumeIntent, FLAG_UPDATE_CURRENT)
+        }
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        curNotificationBuilder.javaClass.getDeclaredField("mActions").apply { 
+            isAccessible = true
+            set(curNotificationBuilder,ArrayList<NotificationCompat.Action>())
+        }
+        curNotificationBuilder = baseNotificationBuilder
+            .addAction(R.drawable.ic_pause_black_24dp,notificationActionText,pendingIntent)
+        notificationManager.notify(NOTIFICATION_ID,curNotificationBuilder.build())
+    }
     @SuppressLint("MissingPermission")
     private fun updateLocationTracking(isTracking: Boolean) {
         if (isTracking) {
@@ -201,28 +236,34 @@ class TrackingService : LifecycleService() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createNotificationChannel(notificationManager)
         }
-        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
-            .setAutoCancel(false)//prevent that user click on notification to be active prevent cancalable
-            .setOngoing(true) // can be swiped away
-            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
-            .setContentTitle("Running App")
-            .setContentText("00:00:00")
-            .setContentIntent(getMainActivityPendingIntent())
+//        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+//            .setAutoCancel(false)//prevent that user click on notification to be active prevent cancalable
+//            .setOngoing(true) // can be swiped away
+//            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+//            .setContentTitle("Running App")
+//            .setContentText("00:00:00")
+//            .setContentIntent(getMainActivityPendingIntent())
 
-        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+        startForeground(NOTIFICATION_ID, baseNotificationBuilder.build())
+        
+        timeRunInSeconds.observe(this, Observer { 
+            val notification  = curNotificationBuilder
+                .setContentText(TrackingUtility.getFormattedStopWatchTime(it * 1000L))
+            notificationManager.notify(NOTIFICATION_ID,notification.build())
+        })
     }
 
-    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
-        this,
-        0,
-        Intent(
-            this, MainActivity::class.java
-        ).also {
-            it.action = ACTION_SHOW_TRACKING_FRAGMENT
-        },
-        FLAG_UPDATE_CURRENT //wehener we lauch the intent it will be updated instead of creating again
-
-    )
+//    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+//        this,
+//        0,
+//        Intent(
+//            this, MainActivity::class.java
+//        ).also {
+//            it.action = ACTION_SHOW_TRACKING_FRAGMENT
+//        },
+//        FLAG_UPDATE_CURRENT //wehener we lauch the intent it will be updated instead of creating again
+//
+//    )
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel(notificationManager: NotificationManager) {
